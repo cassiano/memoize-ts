daa0bc2e66751cce6d9c3525d65a7293dcbf5e44
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2024-07-07 20:25:53 -0300

    Adding more tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,7 @@
-import { assertEquals, assertNotEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts'
+import {
+  assertEquals,
+  assertNotEquals,
+} from 'https://deno.land/std@0.224.0/assert/mod.ts'
 import { memoize, compareValues } from '../src/memoize.ts'
 
 Deno.test('Comparing values', () => {
@@ -30,7 +33,10 @@ Deno.test('Comparing values', () => {
   assertEquals(compareValues([1], [1]), true)
   assertEquals(compareValues([1, 2], [1, 2]), true)
   assertEquals(compareValues([1, 2, 3], [1, 2, 3]), true)
-  assertEquals(compareValues([1, 2, 3, [4, [5, [6]]]], [1, 2, 3, [4, [5, [6]]]]), true)
+  assertEquals(
+    compareValues([1, 2, 3, [4, [5, [6]]]], [1, 2, 3, [4, [5, [6]]]]),
+    true,
+  )
   assertEquals(compareValues([1, 2, 3], [1, 2]), false) // `left` having more elements than `right`.
   assertEquals(compareValues([1, 2, 3], [1, 2, 3, 4]), false) // `left` having less elements than `right`.
 
@@ -39,21 +45,42 @@ Deno.test('Comparing values', () => {
   assertEquals(compareValues({ x: 1 }, { x: 1 }), true)
   assertEquals(compareValues({ x: 1, y: 2, z: 3 }, { z: 3, x: 1, y: 2 }), true) // Different order.
   assertEquals(compareValues({ x: 1, y: 2, z: 3 }, { x: 1, y: 2 }), false) // `left` having more key+value pairs than `right`.
-  assertEquals(compareValues({ x: 1, y: 2, z: 3 }, { x: 1, y: 2, z: 3, w: 4 }), false) // `left` having less key+value pairs than `right`.
   assertEquals(
-    compareValues({ x: 1, y: 2, z: [1, 2, 3, [4, [5, [6]]]] }, { z: [1, 2, 3, [4, [5, [6]]]], x: 1, y: 2 }),
-    true
+    compareValues({ x: 1, y: 2, z: 3 }, { x: 1, y: 2, z: 3, w: 4 }),
+    false,
+  ) // `left` having less key+value pairs than `right`.
+  assertEquals(
+    compareValues(
+      { x: 1, y: 2, z: [1, 2, 3, [4, [5, [6]]]] },
+      { z: [1, 2, 3, [4, [5, [6]]]], x: 1, y: 2 },
+    ),
+    true,
   )
 
   // Objects with array elements.
-  assertEquals(compareValues({ x: [1, 2, 3], y: 4 }, { x: [1, 2, 3], y: 4 }), true)
+  assertEquals(
+    compareValues({ x: [1, 2, 3], y: 4 }, { x: [1, 2, 3], y: 4 }),
+    true,
+  )
 
   // Arrays with object elements.
-  assertEquals(compareValues([1, 2, { x: [3, 4, 5], y: 6 }], [1, 2, { y: 6, x: [3, 4, 5] }]), true)
+  assertEquals(
+    compareValues(
+      [1, 2, { x: [3, 4, 5], y: 6 }],
+      [1, 2, { y: 6, x: [3, 4, 5] }],
+    ),
+    true,
+  )
 
   // Arrays x objects with sequencial integer (natural, ≥ 0) indexes.
-  assertEquals(compareValues([10, 20, 30, 40, 50], { 0: 10, 1: 20, 2: 30, 3: 40, 4: 50 }), true)
-  assertEquals(compareValues([10, 20, 30, 40, 50], { 4: 50, 3: 40, 2: 30, 1: 20, 0: 10 }), true) // Different key order.
+  assertEquals(
+    compareValues([10, 20, 30, 40, 50], { 0: 10, 1: 20, 2: 30, 3: 40, 4: 50 }),
+    true,
+  )
+  assertEquals(
+    compareValues([10, 20, 30, 40, 50], { 4: 50, 3: 40, 2: 30, 1: 20, 0: 10 }),
+    true,
+  ) // Different key order.
 
   // Classes.
 
@@ -110,152 +137,184 @@ Deno.test('Memoizing 1 parameter functions', () => {
 Deno.test('Memoizing factorial() function', () => {
   let calculations = 0
 
-  const factorial = memoize(
-    (n: number): number => (calculations++, n <= 1 ? 1 : n * factorial(n - 1)),
-    ([a], [b]) => a === b
+  const factorial = (n: number): number => (n <= 1 ? 1 : n * factorial(n - 1))
+
+  const memoizedFactorial = memoize(
+    (n: number): number => (
+      calculations++, n <= 1 ? 1 : n * memoizedFactorial(n - 1)
+    ),
+    ([a], [b]) => a === b,
   )
 
   assertEquals(calculations, 0)
 
-  factorial(10)
+  assertEquals(memoizedFactorial(10), factorial(10))
   assertEquals(calculations, 10)
 
-  factorial(10)
+  memoizedFactorial(10)
   assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  factorial(10)
+  memoizedFactorial(10)
   assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  factorial(9)
+  memoizedFactorial(9)
   assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  factorial(8)
+  memoizedFactorial(8)
   assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  factorial(11)
+  assertEquals(memoizedFactorial(11), factorial(11))
   assertEquals(calculations, 11)
 
-  factorial(11)
+  memoizedFactorial(11)
   assertEquals(calculations, 11) // Assures no further calculations were needed.
 })
 
 Deno.test('Memoizing fibonacci() function', () => {
   let calculations = 0
 
-  const fibonacci = memoize(
-    (n: number): number => (calculations++, n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2))
+  const fibonacci = (n: number): number =>
+    n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2)
+
+  const memoizedFibonacci = memoize(
+    (n: number): number => (
+      calculations++,
+      n <= 1 ? n : memoizedFibonacci(n - 1) + memoizedFibonacci(n - 2)
+    ),
   )
 
   assertEquals(calculations, 0)
 
-  fibonacci(0)
+  assertEquals(memoizedFibonacci(0), fibonacci(0))
   assertEquals(calculations, 1)
 
-  fibonacci(1)
+  assertEquals(memoizedFibonacci(1), fibonacci(1))
   assertEquals(calculations, 2)
 
-  fibonacci(2)
+  assertEquals(memoizedFibonacci(2), fibonacci(2))
   assertEquals(calculations, 3)
 
-  fibonacci(3)
+  assertEquals(memoizedFibonacci(3), fibonacci(3))
   assertEquals(calculations, 4)
 
-  fibonacci(4)
+  assertEquals(memoizedFibonacci(4), fibonacci(4))
   assertEquals(calculations, 5)
 
-  fibonacci(4)
+  memoizedFibonacci(4)
   assertEquals(calculations, 5) // Assures no further calculations were needed.
 
-  fibonacci(3)
+  memoizedFibonacci(3)
   assertEquals(calculations, 5) // Assures no further calculations were needed.
 
-  fibonacci(2)
+  memoizedFibonacci(2)
   assertEquals(calculations, 5) // Assures no further calculations were needed.
 
-  fibonacci(1)
+  memoizedFibonacci(1)
   assertEquals(calculations, 5) // Assures no further calculations were needed.
 
-  fibonacci(0)
+  memoizedFibonacci(0)
   assertEquals(calculations, 5) // Assures no further calculations were needed.
+
+  assertEquals(memoizedFibonacci(40), fibonacci(40))
+  assertEquals(calculations, 41)
 })
 
-Deno.test('Memoizing knapsack() function, using a single object parameter', () => {
-  type KnapsackArgsType = {
-    n: number
-    w: number
-    profits: number[]
-    weights: number[]
-  }
+Deno.test(
+  'Memoizing knapsack() function, using a single object parameter',
+  () => {
+    type KnapsackArgsType = {
+      n: number
+      w: number
+      profits: number[]
+      weights: number[]
+    }
 
-  let calculations = 0
+    let calculations = 0
 
-  const knapsack = memoize(({ n, w, profits, weights }: KnapsackArgsType): number => {
-    calculations++
+    const knapsack = memoize(
+      ({ n, w, profits, weights }: KnapsackArgsType): number => {
+        calculations++
 
-    if (n === 0 || w === 0) return 0 // Base case.
+        if (n === 0 || w === 0) return 0 // Base case.
 
-    return Math.max(
-      weights[n - 1] <= w
-        ? knapsack({ n: n - 1, w: w - weights[n - 1], profits, weights }) + profits[n - 1]
-        : 0,
-      knapsack({ n: n - 1, w, profits, weights })
+        return Math.max(
+          weights[n - 1] <= w
+            ? knapsack({ n: n - 1, w: w - weights[n - 1], profits, weights }) +
+                profits[n - 1]
+            : 0,
+          knapsack({ n: n - 1, w, profits, weights }),
+        )
+      },
     )
-  })
 
-  assertEquals(calculations, 0)
+    assertEquals(calculations, 0)
 
-  knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 9)
+    knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 9)
 
-  knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 9) // Assures no further calculations were needed.
+    knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 9) // Assures no further calculations were needed.
 
-  knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 9) // Assures no further calculations were needed.
+    knapsack({ n: 3, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 9) // Assures no further calculations were needed.
 
-  knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 10)
+    knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 10)
 
-  knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 10) // Assures no further calculations were needed.
+    knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
-  assertEquals(calculations, 10) // Assures no further calculations were needed.
-})
+    knapsack({ n: 4, w: 2, profits: [10, 20, 0], weights: [1, 1, 1] })
+    assertEquals(calculations, 10) // Assures no further calculations were needed.
+  },
+)
 
-Deno.test('Memoizing knapsack() function, using a single tuple parameter', () => {
-  type KnapsackArgsType = [n: number, w: number, profits: number[], weights: number[]]
+Deno.test(
+  'Memoizing knapsack() function, using a single tuple parameter',
+  () => {
+    type KnapsackArgsType = [
+      n: number,
+      w: number,
+      profits: number[],
+      weights: number[],
+    ]
 
-  let calculations = 0
+    let calculations = 0
 
-  const knapsack = memoize(([n, w, profits, weights]: KnapsackArgsType): number => {
-    calculations++
+    const knapsack = memoize(
+      ([n, w, profits, weights]: KnapsackArgsType): number => {
+        calculations++
 
-    if (n === 0 || w === 0) return 0 // Base case.
+        if (n === 0 || w === 0) return 0 // Base case.
 
-    return Math.max(
-      weights[n - 1] <= w ? knapsack([n - 1, w - weights[n - 1], profits, weights]) + profits[n - 1] : 0,
-      knapsack([n - 1, w, profits, weights])
+        return Math.max(
+          weights[n - 1] <= w
+            ? knapsack([n - 1, w - weights[n - 1], profits, weights]) +
+                profits[n - 1]
+            : 0,
+          knapsack([n - 1, w, profits, weights]),
+        )
+      },
     )
-  })
 
-  assertEquals(calculations, 0)
+    assertEquals(calculations, 0)
 
-  knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 9)
+    knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 9)
 
-  knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 9) // Assures no further calculations were needed.
+    knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 9) // Assures no further calculations were needed.
 
-  knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 9) // Assures no further calculations were needed.
+    knapsack([3, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 9) // Assures no further calculations were needed.
 
-  knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 10)
+    knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 10)
 
-  knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 10) // Assures no further calculations were needed.
+    knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 10) // Assures no further calculations were needed.
 
-  knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
-  assertEquals(calculations, 10) // Assures no further calculations were needed.
-})
+    knapsack([4, 2, [10, 20, 0], [1, 1, 1]])
+    assertEquals(calculations, 10) // Assures no further calculations were needed.
+  },
+)
